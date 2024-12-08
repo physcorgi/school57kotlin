823257d39c0819d1a0c114e04dbfb03935a9ab98
Dan <76772649+physcorgi@users.noreply.github.com>
2024-12-09 00:24:14 +0300

    fix

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,7 @@ rootProject.name = "school57kotlin"
 //include("lesson3")
 //include("lesson4")
 include("lesson5")
-include("lesson6")
+//include("lesson6")
 include("lesson7")
 include("simple")
 include("lesson8")
